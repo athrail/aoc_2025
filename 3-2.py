a176b4b50605677cefbc4f0655a5ff05a8e593a8
Krzysztof Lewandowski <krzlew1@gmail.com>
2025-12-18 15:02:05 +0100

    day3: wip for part 2

---
+++ 3-2.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+
+def get_bank_max_joltage(bank: str) -> int:
+    digits = [int(c) for c in bank.rstrip()]
+    count = len(digits)
+    print(digits)
+    indexes = []
+    print(indexes)
+
+    indexes.append(0)
+    for i, d in enumerate(digits[1:])
+
+
+    value = "".join([str(digits[i]) for i in indexes])
+    print(f"{value}")
+    return int(value)
+
+
+def sum_joltage(banks: list[str]) -> int:
+    total = 0
+
+    for bank in banks:
+        total += get_bank_max_joltage(bank)
+
+    return total
+
+
+def test_day3():
+    banks = """
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+"""
+    banks = banks.strip()
+    split_banks = banks.splitlines()
+
+    assert 3121910778619 == sum_joltage(split_banks)
+
+
+def main():
+    with open("./input_day3.txt", "r") as f:
+        banks = f.readlines()
+
+    print(f"Total joltage is {sum_joltage(banks)}")
+
+
+test_day3()
